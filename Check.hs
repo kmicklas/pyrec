2505540799b4d8ef72f0b9ad638b5dc9230d1308
Ken Micklas <kmicklas@gmail.com>
2014-02-10 17:57:00 -0500

    Clean up haskell

--- Check.hs
+++ Check.hs
@@ -4,7 +4,11 @@ import Data.Map as M
 import Data.Map (Map)
 
 type Loc = Int
-type Id = (String, Loc)
+
+data Id
+  = Name Loc String
+  | Gen String
+  deriving (Eq, Show, Ord)
 
 data LExpr = At Loc Expr Expr deriving Eq
 data TExpr = As Expr Expr deriving Eq
@@ -12,14 +16,14 @@ data TExpr = As Expr Expr deriving Eq
 type Expr = PExpr LExpr
 
 data PExpr e
-  = Let (Bind e) e
-  | Graph [Bind e] e
+  = Let (Decl e) e
+  | Graph [Decl e] e
   | Assign Id e
   | Cases e [Case e]
   | Try e e
   | Fun [Id] e
   | App e [e]
-  | Id Id
+  | Ident Id
   | Number Double
   | Str String
   | Error String e
@@ -28,25 +32,25 @@ data PExpr e
   | TAny
   | TNum
   | TStr
-  | TFun [Id] [Arg e] e
+  | TFun [Id] [Bind e] e
   deriving Eq
 
-data Bind e
-  = Val Id e e
-  | Var Id e e
+data Decl e
+  = Val (Bind e) e
+  | Var (Bind e) e
   | Data Id [Id] [Variant e]
   deriving Eq
 
-data Arg e = Arg Id e deriving Eq
+data Bind e = Bind Id e deriving Eq
 
-data Case e = Case Pattern e deriving Eq
+data Case e = Case (Pattern e) e deriving Eq
 
-data Pattern
-  = Constr Id (Maybe [Pattern])
-  | Bind Id
+data Pattern e
+  = Constr Id (Maybe [Pattern e])
+  | Binding (Bind e)
   deriving Eq
 
-data Variant e = Variant Id (Maybe [Arg e]) deriving Eq
+data Variant e = Variant Id (Maybe [Bind e]) deriving Eq
 
 
 
@@ -55,17 +59,16 @@ data Variant e = Variant Id (Maybe [Arg e]) deriving Eq
 type Env = Map Id LExpr
 
 eval :: LExpr -> Env -> LExpr
-eval (At l et e) env = At l et $ evalE e env
-
-evalE :: Expr -> Env -> Expr
-evalE (Let (Val n v) e) = eval e $ insert n (eval v env) env
-evalE (Let (Var n v) e) = Let (Var n (eval v env)) $ e
-evalE e = e
+eval (At _ _ (Let (Val (Bind id _) v) e)) env = eval e $ insert id (eval v env) env
+eval e@(At l t (Ident id)) env =
+  case M.lookup id env of
+    Just v  -> v
+    Nothing -> At l t $ Error (show id ++ " not bound") e
+eval e@(At l t _) _ = At l t $ Error ("Don't know how to handle") e
 
 check :: LExpr -> Expr -> Env -> LExpr
 check (At l et e) t env = At l t' e'
   where (As t' e') = tc e (unify t et env) env
-
 
 tc :: Expr -> Expr -> Env -> TExpr
 
@@ -73,7 +76,7 @@ tc (Let (Val n v) e) t env = As t' (Let (Val n v') e')
   where v'@(At _ vt _) = check v TUnknown env
         e'@(At _ t' _) = check e t $ M.insert n vt env
 
-tc (Let (Var n v) e) t env = As t' (Let (Var n v') e')
+tc (Let (Var n _ v) e) t env = As t' (Let (Var n v') e')
   where v'@(At _ vt _) = check v TUnknown env
         e'@(At _ t' _) = check e t $ M.insert n vt env
 
@@ -82,9 +85,9 @@ tc (Assign n v) t env = As t' v'
 
 tc (App f as) t env = undefined
 
-tc e@(Id n) t env = case M.lookup n env of
-                      Just (At _ nt _) -> As (unify t nt env) e
-                      Nothing -> As t e
+tc e@(Ident n) t env = case M.lookup n env of
+                         Just (At _ nt _) -> As (unify t nt env) e
+                         Nothing -> As t e
 
 unify :: Expr -> Expr -> Env -> Expr
 unify TUnknown t _ = t
