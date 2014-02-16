da32f34b94d891f700e5491bc6da10878dc08ba5
John Ericson <Ericson2314@Yahoo.com>
2014-02-15 23:19:32 -0500

    interp is more generic

--- haskell/Pyrec/Interp.hs
+++ haskell/Pyrec/Interp.hs
@@ -16,25 +16,21 @@ import Data.Maybe
 
 import Pyrec.AST
 
+type Env id = Map id (Int, Bool)
 
-type Id = String
+data Store e = Store Int (IntMap e) -- int for bump pointer malloc
 
-newtype Expr = E (PExpr Id Expr)
+type Eval id e m = Env id -> e -> m e
 
-type Env = Map Id (Int, Bool)
-
-data Store = Store Int (IntMap Expr) -- int for bump pointer malloc
-
-
-emptyHeap :: MonadState Store m => m ()
+emptyHeap :: MonadState (Store e) m => m ()
 emptyHeap = put $ Store 0 IM.empty
 
-load :: MonadState Store m => Int -> m Expr
+load :: MonadState (Store e) m => Int -> m e
 load i = do (Store _ m) <- get
             return $ m IM.! i
 
 -- ASSUMES e IS EVALUATEED ALREADY!
-storeNew :: MonadState Store m => Expr -> m Int
+storeNew :: MonadState (Store e) m => e -> m Int
 storeNew e = do (Store i _) <- get
                 let i' = i + 1
                 store i' e
@@ -42,26 +38,29 @@ storeNew e = do (Store i _) <- get
                 put $ Store i' m'
                 return i'
 
-store :: MonadState Store m => Int -> Expr -> m ()
+store :: MonadState (Store e) m => Int -> e -> m ()
 store i e = do (Store i2 m) <- get
                put $ Store i2 $ IM.insert i e m
 
 
-bind :: MonadState Store m => Env -> Id -> Expr -> Expr -> Bool -> m Expr
-bind env iden be reste isMut = do
-    bv  <- eval env be
+bind :: (MonadState (Store e) m, Ord id) => Eval id e m ->
+        Env id -> id -> e -> e -> Bool -> m e
+bind eval' env iden be reste isMut = do
+    bv  <- eval' env be
     ind <- storeNew bv
-    eval (M.insert iden (ind, isMut) env) reste
+    eval' (M.insert iden (ind, isMut) env) reste
 
-eval :: MonadState Store m => Env -> Expr -> m Expr
-eval env (E e) = case e of
-  (Let (Val (Bind iden be)) reste) -> bind env iden be reste False
-  (Let (Var (Bind iden be)) reste) -> bind env iden be reste True
+eval :: (MonadState (Store e) m, Ord id) =>
+        Eval id e m -> (PExpr id e -> e) -> (String -> id) ->
+        Env id -> PExpr id e -> m e
+eval eval' mkE mkID env e = case e of
+  (Let (Val (Bind iden be)) reste) -> bind eval' env iden be reste False
+  (Let (Var (Bind iden be)) reste) -> bind eval' env iden be reste True
 
   (Assign iden expr) -> let
     (ptr, isMut) = env M.! iden
     in if isMut
        then error "Cannot mutate non-variable"
-       else do bv <- eval env expr
+       else do bv <- eval' env expr
                store ptr bv
-               eval env $ E $ Ident "__Nothing"
+               eval' env $ mkE $ Ident $ mkID "__Nothing"
