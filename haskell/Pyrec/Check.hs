c0e53056f393d37dfbd16e1a1b752e07fa3c3793
John Ericson <Ericson2314@Yahoo.com>
2014-02-15 23:32:47 -0500

    Using case..of to tidy pattern matching

--- haskell/Pyrec/Check.hs
+++ haskell/Pyrec/Check.hs
@@ -8,20 +8,17 @@ fixType :: LExpr -> Type -> Env -> LExpr
 fixType (At l et e) t env = tc (At l (unify et t env) e) env
 
 tc :: LExpr -> Env -> LExpr
+tc (At l t e) env = case e of
+  (Def (Let (Val (Bind id vt) v)) e) -> tcLet l t id vt v e env Val
+  (Def (Let (Var (Bind id vt) v)) e) -> tcLet l t id vt v e env Var
 
-tc (At l t (Def (Let (Val (Bind id vt) v)) e)) env =
-  tcLet l t id vt v e env Val
+  (Assign id v) -> At l t $ Assign id' $ fixType v t' env
+    where (id', t') = resolve id env
 
-tc (At l t (Def (Let (Var (Bind id vt) v)) e)) env =
-  tcLet l t id vt v e env Var
+  (Ident id) -> At l (unify t t' env) $ Ident id'
+    where (id', t') = resolve id env
 
-tc (At l t (Assign id v)) env = At l t $ Assign id' $ fixType v t' env
-  where (id', t') = resolve id env
-
-tc (At l t (Ident id)) env = At l (unify t t' env) $ Ident id'
-  where (id', t') = resolve id env
-
-tc (At l t (Number n)) env = At l (unify t TNum env) $ Number n
+  (Number n) -> At l (unify t TNum env) $ Number n
 
 tcLet l t id vt v e env kind = At l t $ Def newDef $ fixType e t newEnv
   where vc@(At _ vt _) = fixType v (checkT vt env) env
