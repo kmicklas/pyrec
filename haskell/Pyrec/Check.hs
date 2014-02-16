aba62207c5ce7f54bc4ca6cc87579a89cd1c176a
Ken Micklas <kmicklas@gmail.com>
2014-02-15 21:44:29 -0500

    Do basic type checking stuff

--- haskell/Pyrec/Check.hs
+++ haskell/Pyrec/Check.hs
@@ -4,13 +4,33 @@ import Pyrec.AST
 
 type Env = [Def LExpr]
 
-tc :: LExpr -> Type -> Env -> LExpr
-tc (At l et (Def (Let (Val (Bind id vt) v)) e)) t env =
-  let ut             = unify et t env
-      vc@(At _ vt _) = tc v (checkT vt env) env
-      newDef         = Let (Val (Bind id vt) vc)
+fixType :: LExpr -> Type -> Env -> LExpr
+fixType (At l et e) t env = tc (At l (unify et t env) e) env
+
+tc :: LExpr -> Env -> LExpr
+
+tc (At l t (Def (Let (Val (Bind id vt) v)) e)) env =
+  tcLet l t id vt v e env Val
+
+tc (At l t (Def (Let (Var (Bind id vt) v)) e)) env =
+  tcLet l t id vt v e env Var
+
+tc (At l t (Assign id v)) env = At l t $ Assign id' $ fixType v t' env
+  where (id', t') = resolve id env
+
+tc (At l t (Ident id)) env = At l (unify t t' env) $ Ident id'
+  where (id', t') = resolve id env
+
+tc (At l t (Number n)) env = At l (unify t TNum env) $ Number n
+
+tcLet l t id vt v e env kind =
+  let vc@(At _ vt _) = fixType v (checkT vt env) env
+      newDef         = Let (kind (Bind id vt) vc)
       newEnv         = newDef : env
-  in At l ut $ Def newDef $ tc e ut newEnv
+  in At l t $ Def newDef $ fixType e t newEnv
+
+resolve :: Id -> Env -> (Id, Type)
+resolve = undefined
 
 checkT :: Type -> Env -> Type
 checkT t env = unify TUnknown t env
