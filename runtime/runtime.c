baf8dd2fdb81965c5053d1d440657efe841527c9
Ken Micklas <kmicklas@gmail.com>
2014-02-18 21:40:26 -0500

    Do arithmetic in runtime

--- runtime/runtime.c
+++ runtime/runtime.c
@@ -6,11 +6,41 @@ typedef struct {
 	mpf_t num;
 } pNumber;
 
-pNumber *loadPyretNumber(double *n) {
-	printf("Loading number: %f\n", *n);
+pNumber *newPyretNumber() {
 	pNumber *ret = malloc(sizeof(pNumber));
-	mpf_init_set_d(ret->num, *n);
+	mpf_init(ret->num);
+	return ret;
+}
+
+pNumber *loadPyretNumber(double *n) {
+	pNumber *ret = newPyretNumber();
+	printf("Loading number: %f\n", *n);
+	mpf_set_d(ret->num, *n);
 	gmp_printf("Loaded: %F\n", ret->num);
+	return ret;
+}
+
+pNumber *pyretPlus(pNumber *a, pNumber *b) {
+	pNumber *ret = newPyretNumber();
+	mpf_add(ret->num, a->num, b->num);
+	return ret;
+}
+
+pNumber *pyretMinus(pNumber *a, pNumber *b) {
+	pNumber *ret = newPyretNumber();
+	mpf_sub(ret->num, a->num, b->num);
+	return ret;
+}
+
+pNumber *pyretTimes(pNumber *a, pNumber *b) {
+	pNumber *ret = newPyretNumber();
+	mpf_mul(ret->num, a->num, b->num);
+	return ret;
+}
+
+pNumber *pyretDivide(pNumber *a, pNumber *b) {
+	pNumber *ret = newPyretNumber();
+	mpf_div(ret->num, a->num, b->num);
 	return ret;
 }
 
