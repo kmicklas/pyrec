20fd4ed10ff9c6bb7cf9010384a85b5b83c28117
John Ericson <Ericson2314@Yahoo.com>
2014-04-09 17:30:01 -0400

    Main compiles again

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -1,37 +1,39 @@
 module Main where
 
-import qualified Data.Map as M
-import           Data.Map (Map)
+import qualified Data.Map          as M
+import           Data.Map               (Map)
 
 import           Pyrec.AST
-import           Pyrec.AST.Parse as P
-import qualified Pyrec.AST.Check as C
+--import           Pyrec.AST.Parse   as P hiding (Type(T))
+import           Pyrec.AST.Desugar as D
+import qualified Pyrec.AST.Check   as C
 
 import           Pyrec.Check
 import           Pyrec.Report
 import           Pyrec.Compile
 import           Pyrec.Emit
 
-ffiEnv =
-  M.fromList [
-    numBinOp 1000 "@pyretPlus",
-    numBinOp 1001 "@pyretMinus",
-    numBinOp 1002 "@pyretTimes",
-    numBinOp 1003 "@pyretDivide"
+ffiEnv = M.fromList $ fmap (uncurry numBinOp) [
+  (1000 , "@pyretPlus"  ),
+  (1001 , "@pyretMinus" ),
+  (1002 , "@pyretTimes" ),
+  (1003 , "@pyretDivide")
   ]
-  where numBinOp l n = (n, Def Val (P.B l n $ T $ TFun [T TNum, T TNum] $ T TNum) ())
+  where numBinOp l n =
+          ( n
+          , Def Val (D.BT l n $ T $ TFun [T TNum, T TNum] $ T TNum) ())
 
-checkEmit :: P.Expr -> String
-checkEmit e = emit $ compile $ snd $ report $ tc ffiEnv e
+checkEmit :: D.Expr -> String
+checkEmit e = emit $ compile $ fst $ report $ tc ffiEnv e
 
 main = putStr $ checkEmit prog4
 
 -- TESTING --
-e l = P.E l TUnknown
+e l = D.E l TUnknown
 
-eLet l n v i = Let (Def Val (P.B l n TUnknown) v) i
-eVar l n v i = Let (Def Var (P.B l n TUnknown) v) i
-eSeq     v i = Let (Def Val (P.B 0 "$temp" TUnknown) v) i
+eLet l n v i = Let (Def Val (D.BT l n TUnknown) v) i
+eVar l n v i = Let (Def Var (D.BT l n TUnknown) v) i
+eSeq     v i = Let (Def Val (D.BT 0 "$temp" TUnknown) v) i
 
 prog1 = e 1 $ eLet 2 "x" (e 3 $ Num 55) $ e 2 $ Ident "x"
 
@@ -40,7 +42,7 @@ prog2 = e 1 $ eVar 2 "x" (e 3 $ Num 55)
                   $ e 8 $ Ident "x"
 
 prog3 = e 1 $ App (e 2 $ Ident "f") $ map (\a -> e (round a) $ Num a) [1..4]
-env3  = M.singleton "f" $ Def Val (P.B 9 "f" $ T $ TFun (replicate 4 $ T TNum) $ T TStr) ()
+env3  = M.singleton "f" $ Def Val (D.BT 9 "f" $ T $ TFun (replicate 4 $ T TNum) $ T TStr) ()
 
 prog4 = e 1 $ eLet 2 "x" (e 3 $ Num 77)
             $ e 4 $ App (e 5 $ Ident "@pyretTimes") [e 6 $ Ident "x", e 7 $ Num 33]
--- haskell/Pyrec/Report.hs
+++ haskell/Pyrec/Report.hs
@@ -16,30 +16,33 @@ import qualified Pyrec.AST.Core  as R
 
 type Errors = [R.ErrorMessage]
 
-report :: C.Expr -> Writer Errors R.Expr
-report (E l t e) = case e of
+report :: C.Expr -> (R.Expr, Errors)
+report = runWriter . rpt
+
+rpt :: C.Expr -> Writer Errors R.Expr
+rpt (E l t e) = case e of
 
   Num n -> return $ oe $ Num n
   Str s -> return $ oe $ Str s
 
-  Fun bds e        -> fmap oe $ Fun bds <$> report e
-  Let d e          -> fmap oe $ Let     <$> mapM report d         <*> report e
-  Graph ds e       -> fmap oe $ Graph   <$> mapM (mapM report) ds <*> report e
-  App f as         -> fmap oe $ App     <$> report f              <*> mapM report as
-  Try e1 bd e2     -> fmap oe $ Try     <$> report e1
-                      <*> return bd <*> report e2
+  Fun bds e        -> fmap oe $ Fun bds <$> rpt e
+  Let d e          -> fmap oe $ Let     <$> mapM rpt d         <*> rpt e
+  Graph ds e       -> fmap oe $ Graph   <$> mapM (mapM rpt) ds <*> rpt e
+  App f as         -> fmap oe $ App     <$> rpt f              <*> mapM rpt as
+  Try e1 bd e2     -> fmap oe $ Try     <$> rpt e1
+                      <*> return bd <*> rpt e2
   Cases vt v cases -> fmap oe $ Cases vt
-                      <$> report v <*> mapM (mapM report) cases
+                      <$> rpt v <*> mapM (mapM rpt) cases
 
   Ident (Bound   _ il is) -> return $ oe $ Ident $ R.Bound il is
   Ident (Unbound      is) -> err $ R.UnboundId is
 
   Assign i v -> case i of
-    Bound Var il is -> (oe . Assign (R.Bound il is)) <$> report v
+    Bound Var il is -> (oe . Assign (R.Bound il is)) <$> rpt v
     Bound Val il is -> err $ R.MutateVar il is
     Unbound      is -> err $ R.UnboundId is
 
---  _ -> R.E l t <$> mapM report e -- so close....
+--  _ -> R.E l t <$> mapM rpt e -- so close....
 
   where oe e = R.E l t e
         
