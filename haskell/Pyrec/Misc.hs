8e50174ef8511fafb0c36706ba23dd935421bba6
John Ericson <Ericson2314@Yahoo.com>
2014-04-08 22:50:43 -0400

    More Pyrec.Misc functions

--- haskell/Pyrec/Misc.hs
+++ haskell/Pyrec/Misc.hs
@@ -1,7 +1,14 @@
 module Pyrec.Misc where
 
 import Control.Monad
+import Control.Applicative
 
+-- | should be pretty self explanatory
 for :: Functor f => f a -> (a -> b) -> f b
 for = flip fmap
 
+-- | like zipWith, but aborts if the lists are not the same length
+map2S :: (a -> b -> r) -> ([a] -> [b] -> Maybe [r])
+map2S _ [] []         = Just []
+map2S f (a:as) (b:bs) = (f a b :) <$> map2S f as bs
+map2S _ _ _           = Nothing
