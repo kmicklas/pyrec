ed9d8b832c8204a241e818c15c9574e674933618
Ken Micklas <kmicklas@gmail.com>
2014-05-07 20:17:50 -0400

    Derive Show instances now that we have PrettyPrint

--- haskell/Pyrec/AST.hs
+++ haskell/Pyrec/AST.hs
@@ -10,37 +10,37 @@ import Text.Parsec.Pos
 
 data Node a
   = Node SourcePos a
-  deriving (Eq, Ord, Functor, Foldable, Traversable)
+  deriving (Eq, Show, Ord, Functor, Foldable, Traversable)
 
 type Id = Node String
 
 data Bind id
   = Bind id (Maybe (Node Type))
-  deriving (Eq, Ord)
+  deriving (Eq, Show, Ord)
 
 data Module
   = Module (Node Provide) [Node Import] Block
-  deriving (Eq, Ord)
+  deriving (Eq, Show, Ord)
 
 data Provide
   = NoProvide
   | ProvideAll
   | ProvideExpr (Node Expr)
-  deriving (Eq, Ord)
+  deriving (Eq, Show, Ord)
 
 data Import
   = Import ModuleName
   | ImportQualified ModuleName Id
-  deriving (Eq, Ord)
+  deriving (Eq, Show, Ord)
 
 data ModuleName
   = Named Id
   | File String
-  deriving (Eq, Ord)
+  deriving (Eq, Show, Ord)
 
 newtype Block
   = Statements [Node Statement]
-  deriving (Eq, Ord)
+  deriving (Eq, Show, Ord)
 
 data Statement
   = ExprStmt (Node Expr)
@@ -50,22 +50,22 @@ data Statement
   | Graph Block
   | FunStmt (Maybe [Id]) Id (Maybe [Bind Id]) (Maybe (Node Type)) Block
   | Data Id (Maybe [Id]) [Variant]
-  deriving (Eq, Ord)
+  deriving (Eq, Show, Ord)
 
 data Variant
   = Variant Id [Bind Id]
-  deriving (Eq, Ord)
+  deriving (Eq, Show, Ord)
 
 data Let id
   = Let (Bind id) (Node Expr)
-  deriving (Eq, Ord)
+  deriving (Eq, Show, Ord)
 
 data Type
   = TIdent Id
   | TFun   [Type] Type
   | TParam [Id]   Type
   | TObject [Bind Id]
-  deriving (Eq, Ord)
+  deriving (Eq, Show, Ord)
 
 data Expr
   = Num Double
@@ -84,18 +84,18 @@ data Expr
   | If [Branch] (Maybe Block)
 
   | TypeConstraint (Node Expr) (Node Type)
-  deriving (Eq, Ord)
+  deriving (Eq, Show, Ord)
 
 data Branch
   = Branch (Node Expr) Block
-  deriving (Eq, Ord)
+  deriving (Eq, Show, Ord)
 
 data Field
   = Immut (Let Key)
   | Mut   (Let Key)
-  deriving (Eq, Ord)
+  deriving (Eq, Show, Ord)
 
 data Key
   = Name Id
   | Dynamic (Node Expr)
-  deriving (Eq, Ord)
+  deriving (Eq, Show, Ord)
