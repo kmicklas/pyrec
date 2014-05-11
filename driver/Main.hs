db4cd76d55ce13426091760170dde517150982fe
John Ericson <Ericson2314@Yahoo.com>
2014-05-11 18:13:39 -0400

    Main prints LLVM now

--- driver/Main.hs
+++ driver/Main.hs
@@ -1,28 +1,31 @@
 module Main where
 
 import           Control.Applicative
-import           Control.Monad.Writer
+import           Control.Monad.RWS
 
-import           Text.Parsec.Error
+import qualified System.IO         as IO
+import qualified System.Exit       as Exit
 
-import qualified System.IO        as IO
-import qualified System.Exit      as Exit
+import           LLVM.General.AST
+import           LLVM.General.PrettyPrint
+
+import           Text.Parsec.Error hiding (Message)
 
 import           Pyrec
 import           Pyrec.PrettyPrint
 
-import           Pyrec.Report     as R
+import           Pyrec.Report      as R
 
 main :: IO ()
-main = pretty True =<< fmap runWriter . parseEmit env <$> getContents
+main = prettyPrint True =<< compile <$> getContents
 
-pretty :: Bool -> Either ParseError (String, Errors) -> IO ()
-pretty exit either = case either of
+prettyPrint :: Bool -> Either ParseError (Module, [Message Error]) -> IO ()
+prettyPrint exit either = case either of
   (Left  err)          -> do
     IO.hPutStrLn IO.stderr $ show err
     when exit Exit.exitFailure
 
   (Right (llvm, errs)) -> do
     forM_ errs $ IO.hPutStrLn IO.stderr . pp
-    IO.hPutStr IO.stdout llvm
+    IO.hPutStr IO.stdout $ showPretty llvm
     when exit Exit.exitSuccess
