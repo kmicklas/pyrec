b3092be1540991d1374708d9b0561fdab5e1871b
John Ericson <Ericson2314@Yahoo.com>
2014-05-13 17:01:50 -0400

    Let can be desugared to Graph

--- src/Pyrec/Compile.hs
+++ src/Pyrec/Compile.hs
@@ -24,7 +24,7 @@ cpsProgram exitName exceptName = cps (n2v exitName, n2v exceptName)
 
 cps :: (Name, Name) -> R.Expr -> CPS Expr
 cps (_,  _)  (R.Error e) = error $ show e
-cps (rk, ek) (R.E _ _ e) = case e of
+cps (rk, ek) (R.E l t e) = case e of
   IR.Num n -> return $ k $ Num n
   IR.Str s -> return $ k $ Str s
 
@@ -36,6 +36,8 @@ cps (rk, ek) (R.E _ _ e) = case e of
                       b' <- cps (rk', ek') b
                       return $ Fix [Fun f argNames (rk', ek') b'] $ k $ Var f
     where argNames = for args $ \ (C.BT l n _) -> Name n l
+
+  IR.Let decl rest -> cps (rk, ek) $ R.E l t $ IR.Graph [decl] rest
 
   where k :: Val -> Expr
         k = Cont (Var rk)
